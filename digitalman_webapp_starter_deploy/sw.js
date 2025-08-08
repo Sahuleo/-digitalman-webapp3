968292b679aa943aaa32d122962b3565794ffb10
Sahuleo <saha.saadaty@gmail.com>
2025-08-09 00:01:32 +0330

    Add files via upload

---
+++ digitalman_webapp_starter_deploy/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open('dm-v1').then(c=>c.addAll([
+    '/', '/index.html', '/assets/app.js', '/assets/data.json', '/manifest.json'
+  ])));
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
